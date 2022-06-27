71f3b4d188cb4a0772356db09926235a88f3abcd
flumpyz <natalianata2001@mail.ru>
2022-06-27 15:19:01 +0500

    added bottom navigation menu

--- app/src/main/java/com/zotreex/sample_project/MainActivity.kt
+++ app/src/main/java/com/zotreex/sample_project/MainActivity.kt
@@ -3,7 +3,10 @@ package com.zotreex.sample_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.zotreex.sample_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -12,6 +15,17 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        val container =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = container.navController
+        bottomNavigationView.setupWithNavController(navController)
+
+        bottomNavigationView.setOnItemSelectedListener{
+            navController.navigate(it.itemId)
+            return@setOnItemSelectedListener true
+        }
 
     }
 
